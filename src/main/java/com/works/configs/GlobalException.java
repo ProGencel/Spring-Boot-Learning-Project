c8fb50ca80ce4632e2192a932b534f0e1883438e
Hakan ÖZER <hakanozer02@gmail.com>
2026-03-27 20:08:18 +0300

    Validations

---
+++ src/main/java/com/works/configs/GlobalException.java
@@ -0,0 +1,34 @@
+package com.works.configs;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        List<HashMap<String, Object>> errors = parseError(e.getFieldErrors());
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    private List<HashMap<String, Object>> parseError(List<FieldError> fieldErrors) {
+        List<HashMap<String, Object>> errors = new ArrayList<>();
+        for (FieldError error : fieldErrors) {
+            HashMap<String, Object> errorMap = new HashMap<>();
+            errorMap.put("field", error.getField());
+            errorMap.put("message", error.getDefaultMessage());
+            errorMap.put("rejectedValue", error.getRejectedValue());
+            errors.add(errorMap);
+        }
+        return errors;
+    }
+
+}
