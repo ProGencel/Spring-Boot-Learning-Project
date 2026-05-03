dfe5b58630f48958b826e23ee90dc37852b70d4b
Ahmet Efe Gençel <bloggeromeo@gmail.com>
2026-05-03 22:08:06 +0300

    Product islemleri eklendui

--- src/main/java/com/works/mvc/ProductController.java
+++ src/main/java/com/works/mvc/ProductController.java
@@ -1,16 +1,15 @@
 package com.works.mvc;
 
+import com.works.dto.ProductUpdateRequestDto;
 import com.works.entity.Product;
 import com.works.repository.ProductRepository;
 import com.works.service.ProductService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("mvc")
@@ -19,6 +18,7 @@ public class ProductController {
 
     final ProductService productService;
     final ProductRepository productRepository;
+    final HttpServletRequest request;
 
     @GetMapping("product")
     public String product(Model model ,@RequestParam(defaultValue = "1") int page)
@@ -38,9 +38,26 @@ public class ProductController {
     @GetMapping("product/edit/{id}")
     public String edit(@PathVariable Long id, Model model)
     {
+        request.getSession().setAttribute("selectId", id);
         Product product = productRepository.findById(id).get();
         model.addAttribute("product",product);
         return "productUpdate";
+    }
+
+    @PostMapping("product/update")
+    public String update(ProductUpdateRequestDto productUpdateRequestDto) {
+        Long selectId = (long) request.getSession().getAttribute("selectId");
+        productUpdateRequestDto.setId(selectId);
+        productService.update(productUpdateRequestDto);
+        return "redirect:/mvc/product";
+    }
+
+    @GetMapping("product/search")
+    public String search(Model model, @RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "") String q) {
+        Page<Product> productPage = productService.search(q, page-1, "asc");
+        model.addAttribute("productPage", productPage);
+        model.addAttribute("q", q);
+        return "product";
     }
 
 }
